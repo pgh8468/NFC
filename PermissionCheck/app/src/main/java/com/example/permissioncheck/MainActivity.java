a42667ca94c25fbbb274c047783afa0f4ead77aa
mickeyshoes <parody9c@gmail.com>
2020-06-30 01:11:04 +0900

    nfc ndef tag

--- PermissionCheck/app/src/main/java/com/example/permissioncheck/MainActivity.java
+++ PermissionCheck/app/src/main/java/com/example/permissioncheck/MainActivity.java
@@ -8,11 +8,16 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,12 +26,14 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
 
     private NfcAdapter nfcAdapter;
     private NdefMessage ndefMessage;
+    IntentFilter[] writeTagFilter;
     private PendingIntent pendingIntent; // NFC로 전송받은 데이터를 Intent 를 이용하여 다른 액티비티로 넘겨주는 역할
     private final int MY_PERMISSION_NFC_CODE = 1000;
     TextView tv1;
@@ -48,30 +55,31 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         else{
 
             //SINGLE_TOP FLAG 는 현재 Activity에서 Intent를 받게 할 수 있음
-            Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+//            Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(this,0,new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
 
-            ndefMessage = new NdefMessage(new NdefRecord[]{ new MakeNDEFRecord().changeNDEFType("test data", Locale.ENGLISH, true)});
+            IntentFilter tagDect = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+            writeTagFilter = new IntentFilter[] {tagDect};
 
 
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.NFC) != PackageManager.PERMISSION_GRANTED){
-
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.NFC}, MY_PERMISSION_NFC_CODE);
-
-                Log.e("코드야 잘 되고 있니?", new String("hello"));
-
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.NFC)){
-
-                    Snackbar.make(getWindow().getDecorView().getRootView(), "현재 기능을 사용하기 위해서는 NFC의 권한 설정이 필요합니다.", Snackbar.LENGTH_LONG)
-                            .setAction("확인", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.NFC} ,MY_PERMISSION_NFC_CODE);
-                                }
-                            }).show();
-                }
-
-            }
+//            if(ContextCompat.checkSelfPermission(this, Manifest.permission.NFC) != PackageManager.PERMISSION_GRANTED){
+//
+//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.NFC}, MY_PERMISSION_NFC_CODE);
+//
+//                Log.e("코드야 잘 되고 있니?", new String("hello"));
+//
+//                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.NFC)){
+//
+//                    Snackbar.make(getWindow().getDecorView().getRootView(), "현재 기능을 사용하기 위해서는 NFC의 권한 설정이 필요합니다.", Snackbar.LENGTH_LONG)
+//                            .setAction("확인", new View.OnClickListener() {
+//                                @Override
+//                                public void onClick(View v) {
+//                                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.NFC} ,MY_PERMISSION_NFC_CODE);
+//                                }
+//                            }).show();
+//                }
+//
+//            }
 
         }
 
@@ -82,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     protected void onResume() {
         super.onResume();
         if (nfcAdapter != null){
-            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, writeTagFilter, null);
 
             if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())){
 
@@ -104,6 +112,13 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         Tag nfctag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        ndefMessage = new NdefMessage(new NdefRecord[]{ new MakeNDEFRecord().changeNDEFType("test", Locale.ENGLISH, true)});
+        if ( write_tag(ndefMessage, nfctag)){
+            Log.e("success", "congratulations");
+        }
+        else{
+            Log.e("fail", "OMG");
+        }
         if(nfctag != null){
             byte[] tagInfo = nfctag.getId();
             Log.e("tagInfo : ",tagInfo.toString());
@@ -123,17 +138,51 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         return sb.toString();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public boolean write_tag(NdefMessage ndefMessage, Tag tag){
 
-        if(MY_PERMISSION_NFC_CODE == requestCode && grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if(ndef != null){
+                ndef.connect();
+                ndef.writeNdefMessage(ndefMessage);
+                return true;
+            }
+            else{
+                NdefFormatable ndefFormat = NdefFormatable.get(tag);
+                if(ndefFormat != null){
+                    ndefFormat.connect();
+                    ndefFormat.format(ndefMessage);
+                    ndefFormat.close();
+                    return true;
+                }
+                else{
+                    Log.e("여","기까지");
+                    return false;
+                }
+            }
 
-            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
         }
 
-        else{
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.NFC}, MY_PERMISSION_NFC_CODE);
-        }
+        return false;
     }
+
+
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if(MY_PERMISSION_NFC_CODE == requestCode && grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//
+//            return;
+//        }
+//
+//        else{
+//            ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.NFC}, MY_PERMISSION_NFC_CODE);
+//        }
+//    }
 }
